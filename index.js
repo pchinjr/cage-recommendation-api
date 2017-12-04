f3d8bf3eb9011b4e344c476af95c722186961ef8
Paul Chin Jr <pchinjr@gmail.com>
2017-12-04 16:45:36 -0500

    slack integration and returning genres to webhook

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 'use strict';
 const config = require('./config.json');
+const genreList = require('./genreList.json');
 const http = require('http');
 
 exports.movieWebhook = (req, res) => {
   // Get the genre from the request
   let genre = req.body.result.parameters['genre'];
-
   //find genre id from request 
-  
+  const arr = genreList.genres;
+  let obj = arr.find(function (obj) { return obj.name === genre; });
+  let genreId = obj.id;
   // Call the moviedb API
-  callMovieApi(genre).then((output) => {
+  callMovieApi(genreId).then((output) => {
     // Return the results of the movie API to Dialogflow
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ 'speech': output, 'displayText': output }));
@@ -20,14 +22,14 @@ exports.movieWebhook = (req, res) => {
   });
 };
 
-function callMovieApi (genre) {
+function callMovieApi (genreId) {
   return new Promise((resolve, reject) => {
-    
+    console.log(genreId)
     var options = {
       "method": "GET",
       "hostname": "api.themoviedb.org",
       "port": null,
-      "path": `/3/discover/movie?api_key=${config.MOVIE_DB_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=80&with_people=2963`,
+      "path": `/3/discover/movie?api_key=${config.MOVIE_DB_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genreId}&with_people=2963`,
       "headers": {
         "cache-control": "no-cache",
         "postman-token": "d88c8b42-9f0e-6308-d6cc-124386cca508"
@@ -35,7 +37,7 @@ function callMovieApi (genre) {
     };
     var req = http.request(options, function (res) {
       var chunks = [];
-    
+
       res.on("data", function (chunk) {
         chunks.push(chunk);
       });
@@ -43,6 +45,7 @@ function callMovieApi (genre) {
       res.on("end", function () {
         var body = Buffer.concat(chunks);
         var jason = JSON.parse(body);
+        console.log(jason);
         var output = jason.results[0].title;
         resolve(output);
       });
