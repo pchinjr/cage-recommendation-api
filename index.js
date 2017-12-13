f577e64ef4f02f2cffd7e40e326dcb5e1dc9ddd2
Paul Chin Jr <pchinjr@gmail.com>
2017-12-13 16:18:21 -0500

    actions work

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mdb = require('moviedb')(config.MOVIE_DB_KEY);
 
 exports.movieWebhook = (req, res) => {
   processRequest(req).then((output) => {
+    console.log(`3 ${output}`);
     res.setHeader('Content-Type', 'application/json');
     res.send(output);
   }).catch((error) => {
@@ -16,30 +17,48 @@ exports.movieWebhook = (req, res) => {
 
 function processRequest(req) {
   return new Promise( (resolve, reject) => {
-    let action = req.result.action;
-    let parameters = req.result.parameters;
-  
+    let action = req.body.result.action;
+    let parameters = req.body.result.parameters;
+    let contexts = req.body.result.contexts;
     //create handlers for actions 
     const actionHandlers = {
       'input.genre' : () => {
-        let genreId = genreDict[ parameters.genre ]; //?
+        let genreId = genreDict[parameters.genre];
         mdb.discoverMovie({ "with_genres": genreId, "with_people" : 2963 }, (err, res) => {
           if (err) {
             reject(err)
           };
-          const movie = res.results[0]
-          resolve( buildResponse(`You should watch ${movie.title} http://image.tmdb.org/t/p/w185${movie.poster_path}`) )//?
+          const movie = res.results[0];
+          const contextOutput = [{
+            'name': 'costars',
+            'lifespan' : 5,
+            'parameters' : {
+              'movieId' : movie.id
+            }
+          }]; 
+          resolve( buildResponse(`You should watch ${movie.title} http://image.tmdb.org/t/p/w185${movie.poster_path}`, contextOutput) );
         });
       },
+      'input.costars' : () => {
+        var movieId = contexts.find( context => context.name === 'costars').parameters.movieId
+        mdb.movieCredits({ "id": movieId }, (err, res) => {
+         if(err) {
+           reject(err)
+         };
+         const cast = res.cast;
+         resolve( buildResponse(`The cast includes ${cast[1].name}`) );
+        })
+      }
     }
-  
+   
     //run the proper action handler
     actionHandlers[action]();
   
-    function buildResponse(responseToUser) {
+    function buildResponse(responseToUser, contextOutput) {
       return { 
-        speech : responseToUser,
-        displayText : responseToUser
+        'speech' : responseToUser,
+        'displayText' : responseToUser,
+        'contextOut' : contextOutput,
       }
     }  
   });
